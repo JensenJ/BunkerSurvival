692e7d011f409b49a940cff6908e70672c25a93c
JensenJ <jensenbradley@icloud.com>
2020-01-06 19:35:58 +0000

    Player Skills
    Added Basic Player Skills framework functions for adding, removing and manipulating the idea of progress within the game.

---
+++ Assets/Scripts/Player/PlayerSkills.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerSkills : NetworkBehaviour
+{
+    GameObject gameManager = null;
+    [SerializeField] public PlayerSkillController skillController = null;
+
+    [SerializeField] int currentSkillPoints = 0;
+    [SerializeField] List<int> currentSkillIDs = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(skillController != null || gameManager != null)
+        {
+            gameManager = GameObject.FindGameObjectWithTag("GameController");
+            skillController = gameManager.GetComponent<PlayerSkillController>();
+        }
+    }
+
+    //Adds a skill to players list
+    public void AddSkillID(int id)
+    {
+        //If the skill has a valid is and the player does NOT already have it
+        if (skillController.IsValidSkill(id) && !PlayerHasSkill(id))
+        {
+            currentSkillIDs.Add(id);
+        }
+    }
+
+    //Removes a skill from players list
+    public void RemoveSkillID(int id)
+    {
+        if (skillController.IsValidSkill(id) && PlayerHasSkill(id))
+        {
+            currentSkillIDs.Remove(id);
+        }
+    }
+
+    //Function to find if the player possesses the skill id
+    public bool PlayerHasSkill(int id)
+    {
+        for (int i = 0; i < currentSkillIDs.Count; i++)
+        {
+            if(id == currentSkillIDs[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Returns all player skill ids
+    public int[] GetCurrentPlayerSkills()
+    {
+        return currentSkillIDs.ToArray();
+    }
+
+    //Gets the current skill points of the player
+    public int GetCurrentSkillPoint()
+    {
+        return currentSkillPoints;
+    }
+}
+
---
+++ Assets/Scripts/PlayerSkillController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerSkillController : NetworkBehaviour
+{
+    //Array containing all skills
+    [SerializeField] private PlayerSkillData[] playerSkills = null;
+
+    public PlayerSkillData[] GetAllSkills()
+    {
+        return playerSkills;
+    }
+
+    public PlayerSkillData GetSkillDataFromID(int id)
+    {
+        //If skill id is not valid
+        if(playerSkills.Length > id || id < 0)
+        {
+            return playerSkills[0];
+        }
+        else
+        {
+            return playerSkills[id];
+        }
+    }
+
+    public bool IsValidSkill(int id)
+    {
+        for (int i = 0; i < playerSkills.Length; i++)
+        {
+            if(id == i)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+[System.Serializable]
+public struct PlayerSkillData
+{
+    public string skillName;
+    public string skillDescription;
+    public int skillID;
+    public int[] skillRequirementIDs;
+    public int skillCost;
+}
